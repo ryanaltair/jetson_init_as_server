4d6864504fa8e510d2287aa73bd47880b201ad78
Ryanaltair <al_tair@live.com>
2020-02-25 14:11:10 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,9 @@
 #rm desktop
 sudo apt purge ubuntu-desktop -y && sudo apt autoremove -y && sudo apt autoclean
 sudo apt-get remove -y nautilus nautilus-* gnome-power-manager gnome-screensaver gnome-termina* gnome-pane* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common zeitgeist-core libzeitgeist* gnome-control-center gnome-screenshot && sudo apt-get autoremove
+
+sudo apt-get remove --purge libreoffice*
+sudo apt-get remove libreoffice-core
+sudo apt-get remove snapd lightdm cups chromium*
+sudo apt-get remove libcurlpp0
+sudo apt-get -y install network-manager
