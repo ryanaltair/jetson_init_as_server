d63ee984ae3ab8762cf5c60a39afd84bea46a45a
Ryanaltair <al_tair@live.com>
2020-03-02 17:01:13 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -3,13 +3,11 @@ sudo systemctl isolate multi-user.target
 sudo chown root:root / /lib
 #rm desktop
 sudo apt purge ubuntu-desktop -y && sudo apt autoremove -y && sudo apt autoclean
-sudo apt-get remove -y nautilus nautilus-* gnome-power-manager gnome-screensaver gnome-termina* gnome-pane* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common zeitgeist-core libzeitgeist* gnome-control-center gnome-screenshot && sudo apt-get autoremove
-
+sudo apt-get remove -y nautilus nautilus-* gnome-power-manager gnome-screensaver gnome-termina* gnome-pane* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common zeitgeist-core libzeitgeist* gnome-control-center gnome-screenshot 
 sudo apt-get remove --purge libreoffice*
-sudo apt-get remove libreoffice-core
-sudo apt-get remove snapd lightdm cups chromium*
-sudo apt-get remove libcurlpp0
-sudo apt-get -y install network-manager
+sudo apt-get remove libreoffice-core snapd lightdm cups chromium* libcurlpp0
+sudo apt-get install -y network-manager
+sudo apt-get autoremove
 
 rm -rf ~/Desktop
 rm -rf ~/Documents
