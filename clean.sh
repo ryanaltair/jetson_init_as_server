d72abb3ed9498081ee7d4dcf40d78da383675363
Ryanaltair <al_tair@live.com>
2020-03-02 20:31:18 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,26 +1,54 @@
+#!/bin/bash
+
+# don't set -e
+set +e 
+# 1. set boot taget to non-desktop
+# 2. remove desktop related files and packages 
+
 # borrow from https://devtalk.nvidia.com/default/topic/1049161/jetson-nano/can-i-install-ubuntu-server-in-jetson-nano-/
-sudo systemctl isolate multi-user.target
-sudo chown root:root / /lib
-#rm desktop
-sudo apt purge ubuntu-desktop -y && sudo apt autoremove -y && sudo apt autoclean
-sudo apt-get remove -y nautilus nautilus-* gnome-power-manager gnome-screensaver gnome-termina* gnome-pane* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common zeitgeist-core libzeitgeist* gnome-control-center gnome-screenshot 
-sudo apt-get remove --purge libreoffice*
-sudo apt-get remove libreoffice-core snapd lightdm cups chromium* libcurlpp0
-sudo apt-get install -y network-manager
-sudo apt-get autoremove
+set_boot_to_no_desktop() {
+    systemctl isolate multi-user.target
+}
 
-rm -rf ~/Desktop
-rm -rf ~/Documents
-rm -rf ~/Downloads
-rm -rf ~/Public
-rm -rf ~/Videos
-rm -rf ~/Classes
-rm -rf ~/Music
-rm -rf ~/examples.desktop
-rm -rf ~/Templates/
-rm -rf ~/Pictures
-rm -rf ~/VisionWorks-SFM-0.90-Samples
-rm -rf ~/NVIDIA_CUDA-9.0_Samples
+uninstall_desktop_packages() {
+ 
+  #rm desktop
+  apt purge ubuntu-desktop -y && sudo apt autoremove -y && sudo apt autoclean
+  apt-get remove -y nautilus nautilus-* gnome-power-manager gnome-screensaver gnome-termina* gnome-pane* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common zeitgeist-core libzeitgeist* gnome-control-center gnome-screenshot 
+  apt-get remove --purge libreoffice*
+  apt-get remove libreoffice-core snapd lightdm cups chromium* libcurlpp0
+  apt-get install -y network-manager
+  apt-get autoremove
+}
+remove_files() {
+  chown root:root / /lib
+  
+  #rm desktop files
+  rm -rf ~/Desktop
+  rm -rf ~/Documents
+  rm -rf ~/Downloads
+  rm -rf ~/Public
+  rm -rf ~/Videos
+  rm -rf ~/Classes
+  rm -rf ~/Music
+  rm -rf ~/examples.desktop
+  rm -rf ~/Templates/
+  rm -rf ~/Pictures
+  rm -rf ~/VisionWorks-SFM-0.90-Samples
+  rm -rf ~/NVIDIA_CUDA-9.0_Samples
+  
+  # rm sample code 
+  rm -rf /usr/src/*
+}
 
-# rm sample code 
-sudo rm -rf /usr/src/*
+
+set -e
+if [ `whoami` = "root" ];then
+  set_boot_to_no_desktop
+  uninstall_desktop_packages
+  remove_files
+else
+  echo "error: no root user"
+fi
+
+
