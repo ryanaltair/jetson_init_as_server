fa262993ccb84bda45f8966a10a8f728ae21270c
Ryanaltair <al_tair@live.com>
2020-03-03 09:51:00 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -17,6 +17,7 @@ uninstall_desktop_packages() {
   apt-get remove -y nautilus nautilus-* gnome-power-manager gnome-screensaver gnome-termina* gnome-pane* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common zeitgeist-core libzeitgeist* gnome-control-center gnome-screenshot 
   apt-get remove --purge -y libreoffice*
   apt-get remove -y libreoffice-core snapd lightdm cups chromium* libcurlpp0
+  apt-get update
   apt-get install -y network-manager
   apt-get autoremove
 }
